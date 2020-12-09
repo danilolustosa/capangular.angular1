1ea483ef55f2b29021a535f6deb37062d612ffe4
Danilo Lustosa <danilolustosa@gmail.com>
2020-12-09 11:44:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular1';
+  propriedade = 'Dan Lustosa';
+  aluno:Aluno = { idade: 36, matricula: 123, nome: 'Dan Lustosa' };
+
+  fnEnviar(){
+    console.log("Clicou no botão!");
+    console.log(this.aluno);
+  }
+}
+interface Aluno{
+  matricula?:number,
+  nome?:string,
+  idade?:number
 }
